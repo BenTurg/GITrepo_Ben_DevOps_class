5d7de11eec6c6c571a42c1f1af67dbde31ead7f3
BenTurg <ben9371@gmail.com>
2023-01-10 22:06:05 +0200

    Add: new python script

---
+++ script_wirte_to_file_python.py
@@ -0,0 +1,6 @@
+# Create a new file named "myfile.txt"
+with open("file_with_commit_trigger.txt", "a") as file:
+    # Write the message "Hello, World!" to the file
+    file.write("trigger \n")
+    
+
